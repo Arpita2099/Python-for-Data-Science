46636aa023f1bd2637d0c074cd29376c4c0b3342
Arpita Sah <arpitasah750@gmail.com>
2025-08-28 21:54:39 +0545

    Create Data_Type.py
    As, i mentioned in the details about the data types of python in this file. I hope it cover all the details to understand the data types clearly and basic using the python hoe can you code using these king of data types and their importance too.

---
+++ Data_Type.py
@@ -0,0 +1,48 @@
+# Let first know that datatype is the classification of data items.
+
+# Integer Data Types 
+print(12) #It will give output 12
+# Python can handle the long integer upto 1*10^308
+print(1e308) # It will give output 1e+308
+
+# Decimal/Float Data Types
+print(13.88) # It will give output 13.88
+# We can also say that Python can handle larger decimal or float value to
+print(1.7e308) #It will give output 1.7e+308
+
+# Boolean Data Types
+# This Data Types are used to print the True and False in Python.
+print(True)
+print(False)
+
+# Text/String Data Types
+# In this data types you can print the text or string using the print function. For inatance,
+print("Hello I am here to make my coding habits in Python.")
+
+# Complex Data Types
+# In this data types you are able to print the complex number like real and imaginary number together in Python. For instance,
+print(4+5j) # In other programming language or in real life we uses 4+5i in the comple number but in the python in the place of i we have to write j to get the complex number.
+
+# List Data Types
+# List data types in the python helps to print the list of data in the python and it is mutal types of data. For inatance,
+print([10,20,30,40,50])
+
+# Tuple Data Types
+# Tuple data types is used to print the tuple data in the python and it is immutable types of data. For instance,
+print((5,10,15,20))
+
+# Note one think in the mind that list and tuple is not a same thing. This is because list is a kind of mutable data type like it can be canged according to you requirement and needs but tuple is an immutable types of data which cannot be changed. This is the main difference between the list and tuple in the python.
+
+# Sets Data Types
+# Sets is a kind os data types in the python which is also a kind of immutable data types in python. For inatance,
+print({1,3,5,7,9})
+
+# Dictionary Data Types
+# Dictionary is also a kind of data type which is used to give the value and name and it will print. For instance,
+print({'Name':'Arpita Sah', 'Gender': 'Female'})
+
+# Also we have to know about the type() function in the python. For instance,
+# If you want to check the data types of any data just print the below syntax to check it.
+print(type(132)) # It will give you the types of data integer data types like this only you can check any kind of data types using function type().
+
+# In conclusion, this is only about the types of data in the python with the help of it we can code in the python programming language.
