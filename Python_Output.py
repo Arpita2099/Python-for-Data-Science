a3568517245e03d7379ada8a28033c1f25fa730d
Arpita Sah <arpitasah750@gmail.com>
2025-08-28 19:15:55 +0545

    Create Python_Output.py
    Python Output

---
+++ Python_Output.py
@@ -0,0 +1,33 @@
+# While starting the code in the python just remember one thing that python is a case sensative language. So, keep in mind while using the syntax and function in the python.
+# Now I am going to print something using the python. For instance,
+print("Hey Everyone!, Welcome to python cosing language.")
+
+#We acn also print anything in python using single inverted comma. For instance,
+print('Okay, this is the code in python to print output using single inverted comma.')
+
+# Also using the triple inverted comma we can print the output in the python. For instance, 
+print('''
+This is my first code in python using triple inverted comma.
+Here, I can write myultiple sentence using this in the python at once.
+And, it will give the output togethers.
+''')
+# Therefore, this is the about the python output using multiple inverted comma.
+
+#Like above, we can also print the intege, boolen, and floats using the print common in the Python. For instance,
+# For integer.
+print(17) # It will provide you output 17.
+
+#For Float
+print(45.89) # It will provide you output 45.89.
+
+# For Boolen,
+print(True) # It is provide you value True.
+print(False) # It will provide you output False.
+
+# Even you can print multiple things at once. For instance,
+print("Arpita is",True, 100, "%") # It will give you output Arpita is  True 100 %
+
+# If you want to add anything rather than space in between above code then you can use below code.
+print("Arpita is",True, 100, "%",) # It will give you output Arpita is /True/100/%
+
+
