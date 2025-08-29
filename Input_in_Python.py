667ddcc8e123ca115a58b600e9208f5a7f140f13
Arpita Sah <arpitasah750@gmail.com>
2025-08-29 17:47:01 +0545

    Create Input_in_Python.py
    How to take input in the python

---
+++ Input_in_Python.py
@@ -0,0 +1,11 @@
+# Input in python.
+# For instance, Here print the are of rectangle.
+length=int(input("Enter the value of Length: "))
+breadth=int(input("Enter the value of Breadth: "))
+Area=length*breadth
+print("Length is: ",length,",Breadth is:",breadth)
+print("Length is: %d ,Breadth is: %d"%(length,breadth))
+print(f"Length is: {length}, Breadth is:{breadth}")
+print("The Area of Rectange is:",Area)
+print("The Area of Rectange is: %d "%Area)
+print(f"The Area of Rectange is: {Area} ")
