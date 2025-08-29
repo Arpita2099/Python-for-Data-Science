92159a60abc8231045dfc50a85a177f5f37a9e08
Arpita Sah <arpitasah750@gmail.com>
2025-08-29 17:43:17 +0545

    Create Variable.py
    Variable, keyword and identifiers.

---
+++ Variable.py
@@ -0,0 +1,23 @@
+# In python no need to declare variable like other programming langusge but you can give it when you are needed. For instance,
+a=89
+print(a)
+
+s="Arpita Sah"
+print(s)
+
+# Way of writing the variable.
+x=5
+y=10
+z=15
+print(x,y,z)
+
+# Second way of writing variable.
+p,q,r=5,6,7
+print(p,q,r)
+
+# Variable can not be start by number, and special symbol except underscore_
+# Also variable can not be declare using white space.
+# Variable can not be in-built keywords.
+
+# This is all about the variable in the python.
+
