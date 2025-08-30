e976e1183293d3dd1c35cb8248aab6fdb6b212b7
Arpita Sah <arpitasah750@gmail.com>
2025-08-30 23:21:04 +0545

    Create Operators_in_Python.py
    How operators play role in python.

---
+++ Operators_in_Python.py
@@ -0,0 +1,45 @@
+# Arithmetic Operators
+# In python there are arithmetic operators like +,-,*,/,//, %,**.
+# Now see how arithemetic operators work.
+print(10+20) # It will give output: 30
+print(30-10) # It will give output: 20
+print(10*3) # It will give output: 30
+print(50/10) # It will give output: 5.0
+print(67//6) # It will give output: 11
+print(12%5) # It will give output: 2
+print(5**2) # It will give square of 5 output: 25
+
+# Relational Operators
+print(12>5) #Output: True
+print(4<10) #Output:True
+print(10>=5) #Output:True
+print(10<=20) #Output:True
+print(20==50) #Output: False
+print(5!=10) #Output:True
+
+# Logical Operators
+print(1 and 1) # Output:1
+print(1 and 0) # Output:0
+print(0 or 0) # Output:0
+print(1 or 0) # Output:1
+print(not(1)) # Output:False
+print(not(0)) # Output:True
+
+#Bitwise Operators
+# Bitwise and operators 
+print(5 & 6) #Output: 4
+print(5 | 6) #Output: 7
+print(5 ^ 6) #Output: 3
+print(~3) #Output:-4
+print(4 >> 2) #Output: 1
+print(4 << 2) #Output: 16
+
+# Assignment Operators
+a=10 #This is called assignment operators
+b=45 #This is called assignment operators
+print(a+b) # Output: 55
+
+# Membership Operators
+# in/not in
+print('K' in 'Kathmandu') #It will give Output: True
+print('K' not in 'Kathmandu') #It will give Output: False
