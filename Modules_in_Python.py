ec66f6a3a3b69371977efe945e8285aedb716876
Arpita Sah <arpitasah750@gmail.com>
2025-08-31 19:24:17 +0545

    Create Modules_in_Python.py
    This code is about modules in python.

---
+++ Modules_in_Python.py
@@ -0,0 +1,21 @@
+# Modules in Python
+
+# Math
+import math
+print(math.factorial(5)) # This will give you output of factirial of 5: 120
+
+# Keywords
+import keyword
+print(keyword.kwlist) # This will provide you the keywords present in the python.
+
+#random 
+import random
+print(random.randint(1,100)) #This will give any random number from the given range.
+
+#datetime
+import datetime
+print(datetime.datetime.now()) # This will provide you the present date and time of now.
+
+# If you want to check that what are the modules present in the python, you can check using the following code.
+help('modules')
+
