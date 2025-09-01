2a31cac09ce8612dcde165d00c329bb2bb80cef0
Arpita Sah <arpitasah750@gmail.com>
2025-09-01 21:21:13 +0545

    Create Loops_in_Python.py

---
+++ Loops_in_Python.py
@@ -0,0 +1,19 @@
+# While loop
+# Write a program to print table using while loop.
+num=int(input("Enter a number: "))
+i=1
+while i<11:
+  print(f"{num}*{i}={num*i}")
+  i+=1
+
+# While loop with else
+x=1
+while x<4:
+  print(x)
+  x=x+1
+else:
+  print("Limit Cross.")
+  
+# For loop
+for i in range(1,21):
+  print(i)
