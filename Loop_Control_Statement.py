3dfd12f7330d5a1dedcee46983ca74e9bdbe7088
Arpita Sah <arpitasah750@gmail.com>
2025-09-02 21:34:25 +0545

    Create Loop_Control_Statement.py
    Here, I used break, continue, and pass loop control statement in python.

---
+++ Loop_Control_Statement.py
@@ -0,0 +1,27 @@
+# Loop Control Statement
+
+# Break: Search is an example of break which is also known as linear searching.
+for i in range(1,11):
+  if i==6:
+    break
+  print(i) # It will give output: 1-5 only.
+
+# Find the prime number and the range is given by the user.
+lower=int(input("Enter a lower number: "))
+upper=int(input("Enter a upper number: "))
+for i in range(lower,upper+1):
+  for j in range(2,i):
+    if i%j==0:
+      break
+  else:
+    print(i)
+    
+# Continue: Skip product is the example of continue in real life.
+for i in range(1,11):
+  if i==5:
+    continue
+  print(i) # It will give output: 1-10 except 5.
+  
+# Pass
+for i in range(1,11):
+  pass # If you do not want to get error or do not have any code than you can use pass.
