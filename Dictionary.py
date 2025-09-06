35905def06e458e76f23dbf5ff58c971c17f50dc
Arpita Sah <arpitasah750@gmail.com>
2025-09-06 20:35:13 +0545

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+# Dictionary
+# Dictionary in Python is a collection of keys values, used to store data value like a map, which. unlike other data types which hold only a single value as an element.
+# In some languages it is known as map or assosiative arrays.
+# Characterstics
+# Mutable
+# Indexing has no meaning
+# Keys can't be duplicated
+# Key can't be multable items
+
+# Creating dictionary
+# empty dictionary
+d={}
+print(d)
+
+# 1D dictionary
+d1={'name':'Arpita','gender':'male'}
+print(d1)
+
+# with mixed key
+d2={(1,2,3,4,5):1,'Arpita':'Sah'}
+print(d2)
