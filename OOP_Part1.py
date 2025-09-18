a30a2316486adca738b16906dae656bafdb13aba
Arpita Sah <arpitasah750@gmail.com>
2025-09-18 23:34:54 +0545

    Create OOP_Part1.py
    Here is the introduction of class

---
+++ OOP_Part1.py
@@ -0,0 +1,43 @@
+# OOP: Object Oriented Programming.
+# OOP is the highly practicular things.
+# Generality to specificity.
+# Object Oriented Programming concepts:
+# 1. Object
+# 2. Class
+# 3. Polymorphism
+# 4. Encapsulation
+# 5. Inheritance
+# 6. Abstraction
+
+# Class: Class is a blueprint which show that how object can behave.
+# Class is the set of rule which object follow.
+# Class can have two things 1. Data or property 2. Functions or behavior
+# The data types like list, integer, float, dictionary, tuple, set are the built-in class in the python.
+# Here, L is the Object and list is the Class.
+# Class Basic Structure:
+# class car:
+#   color="blue" # data
+#   model="sport" # data
+#   def calculate_avg_speed(km,time):
+#       color="blue" # data
+#       model="sport" # data
+
+L=[1,2,3]
+print(type(L))
+
+# Object: Object is an instance of the class.
+# Object Examples
+# 1. Car-----------> WagorR // wagonr=Cars()
+# 2. Sports--------> Gilli Danda // gillidanda=Sports()
+# 3. Animals-------> Langoor // langoor=Animals()
+
+# Syntax to create an object
+# Objectname=classname()
+
+# Object Literal 
+L=[1,2,3,4,5]
+L=list()
+print(L)
+
+s=str()
+print(s)
